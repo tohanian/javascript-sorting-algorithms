5ab2d85151d616a612bc89ac4435cbb39d9454d2
tohanian <tadehohanian@gmail.com>
2019-09-11 20:48:15 -0400

    Add button component

---
+++ sorting-demo/src/components/Button.vue
@@ -0,0 +1,63 @@
+<template>
+  <button class="button" @click="$emit('click')">
+    <div class="button-content">
+      <div class="button-text">
+        <slot />
+      </div>
+      <div class="hover-transition" />
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 6px 12px;
+  background: none;
+  border: 1px solid #FFD23F;
+  color: #FFD23F;
+  border-radius: 0;
+  outline: none;
+  box-shadow: none;
+  cursor: pointer;
+  font-family: 'Space Mono', monospace;
+  text-transform: lowercase;
+  font-size: 14px;
+  letter-spacing: .2em;
+  .button-content {
+    position: relative;
+    .button-text {
+      z-index: 1;
+      position: relative;
+    }
+    .hover-transition {
+      height: 0;
+      width: calc(100% + 24px);
+      position: absolute;
+      left: -12px;
+      bottom: -6px;
+      background: darken(#FFD23F, 50%);
+      transition: height .2s ease;
+    }
+  }
+  &:hover {
+    .button-content {
+      .hover-transition {
+        height: calc(100% + 12px);
+      }
+    }
+  }
+  &:active {
+    .button-content {
+      .hover-transition {
+        background: darken(#FFD23F, 52%);
+      }
+    }
+  }
+}
+</style>
