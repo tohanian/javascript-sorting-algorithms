4f0688f70c10994e7c5a8b1d138fd9d87e103538
tohanian <tadehohanian@gmail.com>
2019-09-17 19:56:42 -0400

    Add selection sort general implementation

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -1,24 +1,40 @@
 function insertionSort(valuesToSort) {
-	for (let i = 0; i < valuesToSort.length; i++) {
-		let temp = valuesToSort[i];
-		let j = i - 1;
-		while (j >= 0 && valuesToSort[j] > temp) {
-			valuesToSort[j + 1] = valuesToSort[j];
-			j--;
-		}
-		valuesToSort[j + 1] = temp;
-	}
-	return valuesToSort;
+  for (let i = 0; i < valuesToSort.length; i++) {
+    let temp = valuesToSort[i];
+    let j = i - 1;
+    while (j >= 0 && valuesToSort[j] > temp) {
+      valuesToSort[j + 1] = valuesToSort[j];
+      j--;
+    }
+    valuesToSort[j + 1] = temp;
+  }
+  return valuesToSort;
 }
 
 function bubbleSort(valuesToSort) {
-	for (let i = 0; i < valuesToSort.length; i++) {
-		for (let j = 0; j < valuesToSort.length - i - 1; j++) {
-			if (valuesToSort[j] > valuesToSort[j + 1]) {
-				let temp = valuesToSort[j];
-				valuesToSort[j] = valuesToSort[j + 1];
-				valuesToSort[j + 1] = temp;
-			}
-		}
-	}
+  for (let i = 0; i < valuesToSort.length; i++) {
+    for (let j = 0; j < valuesToSort.length - i - 1; j++) {
+      if (valuesToSort[j] > valuesToSort[j + 1]) {
+        let temp = valuesToSort[j];
+        valuesToSort[j] = valuesToSort[j + 1];
+        valuesToSort[j + 1] = temp;
+      }
+    }
+  }
+}
+
+function selectionSort(valuesToSort) {
+  for (let i = 0; i < valuesToSort.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < valuesToSort.length; j++) {
+      if (valuesToSort[j] < valuesToSort[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      let temp = valuesToSort[i];
+      valuesToSort[i] = valuesToSort[min];
+      valuesToSort[min] = temp;
+    }
+  }
 }
